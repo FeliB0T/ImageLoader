1121ff72fd3d74000d1d92b3c290be01e055c844
FeliB0T <felipe.matheus121@hotmail.com>
2021-01-23 20:11:33 -0300

    code update

--- src/Tela.java
+++ src/Tela.java
@@ -5,64 +5,97 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Tela {
 
-    public void GetAndClear(TextField textField){
+    public String textoFinal;
+    public URL urlFinal;
+    public TextField textField = new TextField();
+    public JButton jbutton = new JButton();
+
+    public void getAndClear(TextField textField){
+
         String texto = textField.getText();
         textField.setText("");
-        System.out.print(texto);
+        try {
+            tryRequest();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        textoFinal = texto;
+
     }
-
-    public void Tela(){}
-
-    public void criarTela(String titulo){
+    public void criarTela(String titulo) {
         JFrame frame = new JFrame();
-        Rectangle standard = new Rectangle(0,0,450,150);
+        Rectangle standard = new Rectangle(0, 0, 450, 150);
         frame.setVisible(true);
         frame.setBounds(standard);
         frame.setResizable(false);
+
         JPanel jPanel = new JPanel();
         jPanel.setBounds(standard);
         jPanel.setLayout(null);
 
 
+        textField.setBounds(20, 45, 300, 25);
 
-        TextField textField = new TextField();
 
-        textField.setBounds(20,45,300,25);
-
-        JButton jbutton = new JButton();
         jbutton.setText("ir");
-        jbutton.setBounds(320,45,50,25);
+        jbutton.setBounds(320, 45, 50, 25);
+
         jPanel.add(jbutton);
         jPanel.add(textField);
+
         frame.add(jPanel);
 
+    }
+    public void pullRequest(){
         textField.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
+        @Override
+        public void keyTyped(KeyEvent e) {
+        }
+        @Override
+        public void keyPressed(KeyEvent e) {
+            if(e.getKeyCode() == KeyEvent.VK_ENTER){
+                getAndClear(textField);
+                System.out.println(textoFinal);
             }
-            @Override
-            public void keyPressed(KeyEvent e) {
-                if(e.getKeyCode() == KeyEvent.VK_ENTER){
-                     GetAndClear(textField);
-                }
 
-            }
+        }
 
-            @Override
-            public void keyReleased(KeyEvent e) {
+        @Override
+        public void keyReleased(KeyEvent e) {
 
-            }
-        });
+        }
+    });
 
         jbutton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                GetAndClear(textField);
-            }
-        });
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            getAndClear(textField);
+        }
+    });
 
+}
+    public void tryRequest() throws IOException {
+
+        URL url = new URL(textoFinal);
+
+        URLConnection connection = url.openConnection();
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        connection.getInputStream()));
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,30 +1,16 @@
-import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import javax.net.ssl.HttpsURLConnection;
-import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) throws IOException {
         Tela tela = new Tela();
         tela.criarTela("Loader");
-        URL yahoo = new URL("https://www.google.com/");
-        URLConnection yc = yahoo.openConnection();
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(
-                        yc.getInputStream()));
-        String inputLine;
+        tela.pullRequest();
 
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine);
-        in.close();
+
 
     }
 }
