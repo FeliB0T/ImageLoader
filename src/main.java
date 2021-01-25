b821c28d6c98352ee0c032653c5b7cf54431eb98
FeliB0T <felipe.matheus121@hotmail.com>
2021-01-24 22:51:39 -0300

    not used imports deleted

--- src/main.java
+++ src/main.java
@@ -1,11 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-
 public class main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         Tela tela = new Tela();
         tela.criarTela("Loader");
         tela.pullRequest();
